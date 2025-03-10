9d1084955175f6dec1103e8305e015fa58ec29f0
Nicolas Oliveira da Rocha <nicolas1421rocha@gmail.com>
2025-03-10 19:15:46 -0400

    1.8

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={ 
+    return render(request, 'pages/home.html', context={ 
         'name': 'Nicolas Oliveira',
     })
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-def sobre(request):
-    return HttpResponse('sobre2')
